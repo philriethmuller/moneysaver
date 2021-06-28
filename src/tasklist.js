3d27eead38fe7822b2b48cc8bb9d7303318bcef1
Phil Riethmuller <phil.riethmuller@gmail.com>
2021-06-28 21:26:07 +1000

    Added task value to task list

--- src/tasklist.js
+++ src/tasklist.js
@@ -6,10 +6,21 @@ export default function TaskList(props) {
     props.onItemClick(amount);
   }
 
+  const formatCurrency = (amount) => {
+    if (amount < 1) {
+      return `${amount*100}c`;
+    } else {
+      return `$${parseFloat(amount).toFixed(2)}`;
+    }
+  }
+
   return (
-    <ul>
+    <ul class="taskList">
       {props.taskItems.map((item, index) => (
-        <li onClick={() => handleClick(item.amount)}>{item.name}</li>
+        <li onClick={() => handleClick(item.amount)}>
+          <span>{item.name}</span>
+          <span class="taskValue">+{formatCurrency(item.amount)}</span>
+        </li>
       ))}
     </ul>
   );
